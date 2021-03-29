4d62620ac41556dad209d76af5aba806eede8c56
Eunchurn Park <eunchurn.park@gmail.com>
2021-03-29 17:47:06 +0900

    chore(publish): npm

--- packages/ring-ts/src/ring.ts
+++ packages/ring-ts/src/ring.ts
@@ -33,7 +33,7 @@ interface Ring {
  * @method `toArray(): number[]` ring buffer to array
  * @method `forEach(callback: (item: number | undefined) => void): void`: Iterative calculation method
  * @author Eunchurn Park
- * @version 0.0.1
+ * @version 0.0.5
  */
 
 export const Ring = function (this: Ring, size: number) {
