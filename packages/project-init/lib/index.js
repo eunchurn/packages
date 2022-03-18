b32324702ee580b692c6db692c0f63bf19db0124
Eunchurn Park <eunchurn.park@gmail.com>
2022-03-19 00:54:42 +0900

    fix(shelljs): without shelljs

--- packages/project-init/lib/index.js
+++ packages/project-init/lib/index.js
@@ -1,24 +1,42 @@
 #! /usr/bin/env node
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const shelljs_1 = __importDefault(require("shelljs"));
+// import shelljs from "shelljs";
+const child_process_1 = require("child_process");
 const fs_1 = __importDefault(require("fs"));
 const eslintConfig_1 = require("./eslintConfig");
 const prettierConfig_1 = require("./prettierConfig");
 const vscodeConfig_1 = require("./vscodeConfig");
-shelljs_1.default.exec("yarn add -D typescript ts-node ts-node-dev @types/node @eunchurn/base-eslint-config");
-shelljs_1.default.exec("npx gitignore node");
-shelljs_1.default.exec("yarn tsc --init --outDir dist");
-fs_1.default.writeFileSync(".eslintrc.json", JSON.stringify(eslintConfig_1.eslint, null, 2));
-fs_1.default.writeFileSync(".prettierrc", JSON.stringify(prettierConfig_1.prettier, null, 2));
-try {
-    shelljs_1.default.mkdir(".vscode");
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        (0, child_process_1.execSync)("yarn add -D typescript ts-node ts-node-dev @types/node @eunchurn/base-eslint-config");
+        const giResult = (0, child_process_1.execSync)("npx gitignore node");
+        console.log(giResult.toString());
+        const tscResult = (0, child_process_1.execSync)("yarn tsc --init --outDir dist");
+        console.log(tscResult.toString());
+        fs_1.default.writeFileSync(".eslintrc.json", JSON.stringify(eslintConfig_1.eslint, null, 2));
+        fs_1.default.writeFileSync(".prettierrc", JSON.stringify(prettierConfig_1.prettier, null, 2));
+        try {
+            (0, child_process_1.execSync)("mkdir .vscode");
+        }
+        catch (_a) {
+            console.log(".vscode already exist");
+        }
+        fs_1.default.writeFileSync(".vscode/settings.json", JSON.stringify(vscodeConfig_1.vscode, null, 2));
+        return "done";
+    });
 }
-catch (_a) {
-    console.log(".vscode already exist");
-}
-fs_1.default.writeFileSync(".vscode/settings.json", JSON.stringify(vscodeConfig_1.vscode, null, 2));
+main().then(() => { console.log("== 🎉 eunchurn project setting done"); });
 //# sourceMappingURL=index.js.map
--- packages/project-init/src/index.ts
+++ packages/project-init/src/index.ts
@@ -1,20 +1,28 @@
 #! /usr/bin/env node
-import shelljs from "shelljs";
+// import shelljs from "shelljs";
+import { execSync } from "child_process";
 import fs from "fs";
 import { eslint } from "./eslintConfig";
 import { prettier } from "./prettierConfig";
 import { vscode } from "./vscodeConfig";
 
+async function main() {
+  execSync("yarn add -D typescript ts-node ts-node-dev @types/node @eunchurn/base-eslint-config");
+  const giResult = execSync("npx gitignore node");
+  console.log(giResult.toString())
 
-shelljs.exec("yarn add -D typescript ts-node ts-node-dev @types/node @eunchurn/base-eslint-config");
-shelljs.exec("npx gitignore node");
-shelljs.exec("yarn tsc --init --outDir dist")
+  const tscResult = execSync("yarn tsc --init --outDir dist")
+  console.log(tscResult.toString())
 
-fs.writeFileSync(".eslintrc.json", JSON.stringify(eslint, null, 2))
-fs.writeFileSync(".prettierrc", JSON.stringify(prettier, null, 2))
+  fs.writeFileSync(".eslintrc.json", JSON.stringify(eslint, null, 2))
+  fs.writeFileSync(".prettierrc", JSON.stringify(prettier, null, 2))
 
-try {
-  shelljs.mkdir(".vscode");
-} catch { console.log(".vscode already exist") }
+  try {
+    execSync("mkdir .vscode");
+  } catch { console.log(".vscode already exist") }
 
-fs.writeFileSync(".vscode/settings.json", JSON.stringify(vscode, null, 2))
+  fs.writeFileSync(".vscode/settings.json", JSON.stringify(vscode, null, 2))
+  return "done"
+}
+
+main().then(() => { console.log("== 🎉 eunchurn project setting done") })
